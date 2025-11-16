7b5fcc0f3ff8de14001f4a02204c94530eaeb777
Tommer Raz <tommer.raz@gmail.com>
2025-11-16 11:25:03 +0200

    added req file gen step

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from json import load
 from crewai.flow.flow import Flow, listen, start
-from crewai.agent import Agent
-from crewai import LLM
-from pydantic import BaseModel, Field
+from crewai import LLM, Agent
+from pydantic import BaseModel
 from crews.mlops_crew.crew import MLOpsCrew
+from typing import List
 import nbformat
 
-# Define flow state
+class CodeOutput(BaseModel):
+    code: str
+
 class FlowState(BaseModel):
     notebook_path: str = ""
     code: str = ""
@@ -21,8 +22,36 @@ class JupyterToTrainFlow(Flow[FlowState]):
     
     @listen(load_code)
     def refactor_code(self):
-        result = MLOpsCrew().crew().kickoff(inputs={"code": self.state.code})
-        print(result)
+        train_code = MLOpsCrew().crew().kickoff(inputs={"code": self.state.code})
+        return train_code
+    
+    @listen(refactor_code)
+    def create_req_file(self, train_code):
+        class Packages(BaseModel):
+            packages: List[str]
+            
+        llm = LLM(model="ollama/qwen2.5-coder:1.5b",
+                  base_url="http://localhost:11434", 
+                  timeout=1200)
+        agent = Agent(
+            role="Python Dependency Extractor",
+            goal="Extract Python dependencies from the code. Every package you see in the code, you need to extract it and return it as a list of packages. You will not produce any other text than the list.",
+            backstory="You are a Python dependency extractor. You are given a code and you need to extract the dependencies from the code.",
+            llm=llm
+        )
+    
+        result = agent.kickoff(
+            f"Extract all Python package dependencies (imports) from this code: {train_code}",
+            response_format=Packages
+            )
+
+        if result.pydantic:
+            print("result", result.pydantic)
+        else:
+            print("result", result)
+        # Write to file
+        with open("output/requirements.txt", "w") as f:
+            f.write("\n".join(result.pydantic.packages))
         return result
 
 def kickoff():
@@ -33,3 +62,4 @@ def kickoff():
 
 if __name__ == "__main__":
     kickoff()
+
